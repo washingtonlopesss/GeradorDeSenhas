4b4710956ea9590d3ee652e334455e44fe6523e3
Washington Lopes <washington.lopes2003@gmail.com>
2023-08-19 01:49:22 -0300

    Implementação de dark mode e novas funcionalidades

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
 const caracteresPermitidos = 'abcdefghijklmnopqrstuvwxyz0123456789';
+let senhaInterval;
 
 function gerarSenha(comprimento) {
     let senha = '';
-    for (let i = 1; i <= comprimento; i++) {
+    for (let i = 0; i < comprimento; i++) {
         const indice = Math.floor(Math.random() * caracteresPermitidos.length);
         senha += caracteresPermitidos[indice];
     }
     return senha;
 }
 
-document.getElementById('gerarSenha').addEventListener('click', function() {
-    const senhaGerada = gerarSenha(4);
-    document.getElementById('senha').textContent = senhaGerada;
-    document.getElementById('copiarSenha').disabled = false;
-});
-
-document.getElementById('copiarSenha').addEventListener('click', function() {
-    const senha = document.getElementById('senha').textContent;
+function copiarSenha() {
+    const senha = document.getElementById('senha').value;
     const tempInput = document.createElement('textarea');
     tempInput.value = senha;
     document.body.appendChild(tempInput);
     tempInput.select();
-    tempInput.setSelectionRange(0, 99999); 
+    tempInput.setSelectionRange(0, 99999);
     document.execCommand('copy');
     document.body.removeChild(tempInput);
+}
+
+function copiarResultado() {
+    const resultado = document.getElementById('resultado').value; // Correção aqui
+    const tempInput = document.createElement('textarea');
+    tempInput.value = resultado; // Correção aqui
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+}
+
+function converterParaMaiusculas() {
+    const inputElement = document.getElementById('inputString');
+    const resultadoElement = document.getElementById('resultado');
+    const inputString = inputElement.value;
+    const stringConvertida = inputString.toUpperCase();
+    resultadoElement.value = stringConvertida;
+}
+
+document.getElementById('check').addEventListener('click', function() {
+    if (this.checked) {
+        senhaInterval = setInterval(function() {
+            const senhaGerada = gerarSenha(4);
+            document.getElementById('senha').value = senhaGerada;
+        }, 1000);
+    } else {
+        clearInterval(senhaInterval);
+    }
+});
+
+document.getElementById('gerarSenha').addEventListener('click', function() {
+    const senhaGerada = gerarSenha(4);
+    document.getElementById('senha').value = senhaGerada;
+    document.getElementById('copiarSenha').disabled = false;
+});
+
+document.getElementById('copiarSenha').addEventListener('click', copiarSenha);
+
+document.getElementById('copiarResultado').addEventListener('click', copiarResultado);
+
+document.getElementById('inputString').addEventListener('input', converterParaMaiusculas);
+
+document.getElementById('toggleSenha').addEventListener('click', function() {
+    const senhaInput = document.getElementById('senha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        this.textContent = 'Esconder Senha';
+    } else {
+        senhaInput.type = 'password';
+        this.textContent = 'Mostrar Senha';
+    }
 });
