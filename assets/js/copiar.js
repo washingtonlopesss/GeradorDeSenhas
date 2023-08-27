cff3650075dc4c3964706f1031df7c9cb6fe3a7f
Washington Lopes <washington.lopes2003@gmail.com>
2023-08-27 02:04:06 -0300

    Finalizado mensagem de copiar e animação dos botões

--- assets/js/copiar.js
+++ assets/js/copiar.js
@@ -11,9 +11,21 @@ function copiar(campo) {
 document.getElementById('copiarSenha').addEventListener('click', function() {
     const senha = document.getElementById('senha').value
     copiar(senha)
+    const senhaCopiadaElement = document.getElementById('senhaCopiada');
+
+    senhaCopiadaElement.style.display = 'block';
+    setTimeout(function() {
+        senhaCopiadaElement.style.display = 'none';
+    }, 3000);
 })
 
 document.getElementById('copiarTxtConvertido').addEventListener('click', function() {
     const convertido = document.getElementById('convertido').value
     copiar(convertido)
+
+    const txtCopiadoElement = document.getElementById('txtCopiado')
+    txtCopiadoElement.style.display = 'block';
+    setTimeout(function() {
+        txtCopiadoElement.style.display = 'none';
+    }, 3000);
 })
