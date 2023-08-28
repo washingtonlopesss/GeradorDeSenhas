c4c645d5b57e3fc5943c6088f94e96c4847f240f
Washington Lopes <washington.lopes2003@gmail.com>
2023-08-28 16:27:38 -0300

    Resolvendo requisito de funcionalidades

--- assets/js/animacao.js
+++ assets/js/animacao.js
@@ -1,14 +1,27 @@
 const progressBar = document.getElementById('progressBar')
+let tempoTemporizador
+let animating = false
 
 function animacao() {
+    if (animating) {
+        progressBar.style.left = '0%'
+        void progressBar.offsetWidth
+        progressBar.style.animation = 'none'
+        void progressBar.offsetWidth
+        progressBar.style.animation = null
+    }
+
     let valorCapturado = capturarInputs()
-    progressBar.classList.toggle('progress-bar-animation')
-    progressBar.style.setProperty('--animation-duration', valorCapturado + "ms")
+    progressBar.style.setProperty('--animation-duration', valorCapturado + 'ms')
+
+    animating = true
+    progressBar.style.animation = 'none'
+    void progressBar.offsetWidth
+    progressBar.style.animation = null
 }
 
 progressBar.addEventListener('animationiteration', function() {
-        const senhaGerada = gerarSenha(4)
-        document.getElementById('senha').value = senhaGerada
-});
-
-
+    const senhaGerada = gerarSenha(4)
+    document.getElementById('senha').value = senhaGerada
+    animating = false
+})
--- assets/js/atualizarTemporizador.js
+++ assets/js/atualizarTemporizador.js
@@ -1,14 +1,13 @@
-let tempoTemporizador
-
 function atualizarTemporizador() {
     let check = document.getElementById('check').checked
     const boxNumbers = document.getElementById('boxNumbers')
     const progressBar = document.querySelector('.progress-bar')
-    
+
     if (check) {
         boxNumbers.style.display = 'flex'
         progressBar.style.display = 'block'
         clearInterval(tempoTemporizador)
+        animacao()
         tempoTemporizador = setInterval(function() {
             animacao()
         }, capturarInputs())
@@ -16,6 +15,7 @@ function atualizarTemporizador() {
         boxNumbers.style.display = 'none'
         progressBar.style.display = 'none'
         clearInterval(tempoTemporizador)
+        animating = false
     }
 }
 
